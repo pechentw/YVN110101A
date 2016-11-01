a5a6d5c33a2045767ed19bbd3aa33a2c12ade59a
pechentw <pechen.tw@gmail.com>
2016-11-01 21:15:53 +0800

    3nd commit

--- app/src/main/java/com/soho/yvtc/yvn110101a/MainActivity.java
+++ app/src/main/java/com/soho/yvtc/yvn110101a/MainActivity.java
@@ -1,24 +1,32 @@
 package com.soho.yvtc.yvn110101a;
 
 import android.content.DialogInterface;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
+public class MainActivity extends AppCompatActivity {
+    int tmp = -1, ch = -1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void click1(View v)
-    {
+
+    public void click1(View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("對話框測試");
-        builder.setMessage("這是對話框內容\n這是第二行");
+        builder.setMessage("這是對話框內容\n第二行");
 
         builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
             @Override
@@ -37,9 +45,73 @@ public class MainActivity extends AppCompatActivity {
         builder.setNeutralButton("忽略", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(MainActivity.this,"忽略被按下", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "忽略被按下", Toast.LENGTH_LONG).show();
             }
         });
+        builder.show();
+    }
+
+    public void click2(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("輸入對話框測試");
+        builder.setMessage("請輸入你的暱稱");
+        final EditText ed = new EditText(MainActivity.this);
+        builder.setView(ed);
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                TextView tv = (TextView) findViewById(R.id.textView);
+                String str = ed.getText().toString();
+                tv.setText(str);
+            }
+        });
+        builder.show();
+    }
+    public void click3(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("選項對話框測試");
+        builder.setSingleChoiceItems(R.array.drinks, ch, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tmp = which;
+             }
+        });
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            ch = tmp;
+                if (ch >= 0){
+                    String[] drinks = getResources().getStringArray(R.array.drinks);
+                    TextView tv2 = (TextView) findViewById(R.id.textView2);
+                    tv2.setText(drinks[ch]);
+                    Toast.makeText(MainActivity.this, "確定被按下", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                tmp = ch;
+                Toast.makeText(MainActivity.this, "取消被按下", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.show();
+    }
+    public void click4(View v)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("選項對話框測試");
+        builder.setItems(R.array.drinks, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String[] drinks = getResources().getStringArray(R.array.drinks);
+                TextView tv3 = (TextView)findViewById(R.id.textView3);
+                tv3.setText(drinks[which]);
+            }
+        });
+        builder.setCancelable(false);
         builder.show();
     }
 }
